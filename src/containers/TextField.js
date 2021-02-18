2aefb75c7611c0ed0cefe596a00fbaaa9bac0e45
Mustafa-ilahi <mustafailahi586@gmail.com>
2021-02-19 01:47:30 +0500

    some changes

--- src/containers/TextField.js
+++ src/containers/TextField.js
@@ -18,7 +18,7 @@ export default function BasicTextFields() {
 
   return (
     <form className={classes.root} noValidate autoComplete="off">
-      <TextField id="outlined-basic" placeholder=" &nbsp; &nbsp; &nbsp; &nbsp; Pakistan" variant="outlined" style={{backgroundColor: "white", border:"2px solid black",borderRadius:"10px"}}/>
+      <TextField id="outlined-basic" placeholder=" &nbsp; &nbsp; &nbsp; &nbsp; Pakistan" variant="outlined" style={{backgroundColor: "white", border:"2px solid black",borderRadius:"10px",height:"60px",width:"300px"}}/>
       <img src={search_icon} id="search_icon"/>
       <TextField id="outlined-basic" placeholder="Find Cars, Mobile Phone and more..." variant="outlined" style={{backgroundColor: "white",position:"relative",right:"43px",width:"800px",border:"2px solid black",borderRadius:"10px"}}/>
       <img src={search_iconBg} id="search_iconBg"/>      
